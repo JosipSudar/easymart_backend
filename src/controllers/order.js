ff0d6e00cca5e0fef65c81ae18e2f1588b4b5c9f
JosipSudar <josip.sudar@gmail.com>
2024-04-13 19:58:48 +0200

    added custom errors to the controllers, product controller refactoring

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -22,6 +22,7 @@ const getOrders = async (req, res) => {
 const getOrderByUser = async (req, res) => {
   try {
     const { id } = req.params;
+    if (!id) throw new CustomError(400, "User ID is required");
     const orders = await Order.find({ user: id });
     res.status(200).json({ orders });
   } catch (error) {
@@ -32,6 +33,7 @@ const getOrderByUser = async (req, res) => {
 const getOrderById = async (req, res) => {
   try {
     const { id } = req.params;
+    if (!id) throw new CustomError(400, "Order ID is required");
     const order = await Order.findOne({ _id: id });
     res.status(200).json({ order });
   } catch (error) {
